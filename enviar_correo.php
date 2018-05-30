73f7c70b09dfb9ef48d38a692f5041429d13e845
Ing-Anyeliz <39038277+Ing-Anyeliz@users.noreply.github.com>
2018-05-30 17:04:33 -0500

    30052018

---
+++ enviar_correo.php
@@ -0,0 +1,15 @@
+<?php
+
+$destino = "anyelizrico@gmail.com";
+$empresa = $_POST['empresa'];
+$telefono = $_POST['telefono'];
+$nombrepc = $_POST['nombrepc'];
+$correo = $_POST['correo'];
+$cargo = $_POST['cargo'];
+$otros = $_POST['otro'];
+$mensaje = $_POST['mensajes'];
+$contenido = "Empresa: " . $empresa . "\nTeléfono: " . $telefono . "\nContácto: " . $nombrepc . "\nCorreo: " . $correo . "\nCargo: " . $cargo . "\nOtros: " . $otros . "\nMensaje: " .$mensaje;
+mail($destino, "Contácto vía web", $contenido);
+header("Location: mensaje_enviado.html")
+
+ ?>
